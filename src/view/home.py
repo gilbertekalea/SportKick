254fbedd00fa9dcd4beee72bf3f21559b6bb4067
gilbertekalea <gilbertekalea@gmail.com>
2022-08-04 23:08:46 -0400

    stashed

--- src/view/home.py
+++ src/view/home.py
@@ -15,6 +15,7 @@ from .. import db
 from src.forms import RegistrationForm, ViewUserProfileForm, CreatePostForm
 from src.model.schemas import Attributes, Team, Post, Bookmarks
 from src.controller.user import User
+from src.controller.club import ClubName, TeamSchema
 from src.utility import schedule
 from src.utility.email import ManageEmailTemplate
 from src import login_manager, mail
@@ -22,8 +23,10 @@ from flask_login import current_user, login_required, login_user, logout_user
 from src.utility import blogsview
 
 
-home_bp = Blueprint('home', __name__, template_folder="templates")
+home_bp = Blueprint("home", __name__, template_folder="templates")
+
+
 @home_bp.route("/")
 def home_page():
-    available_teams = Team.query.all()
-    return render_template('home_page.html', available_teams=available_teams)
+    available_teams = ClubName.convert_to_json()
+    return render_template("home_page.html", available_teams=available_teams)
